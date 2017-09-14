08a26c9671cc9fd195991ad673de62aeb9f2cfb9
anitaa <anitaa@airloyal.com>
2017-09-14 20:55:33 +0530

    Snack bar implementation

--- app/src/main/java/com/an/inshorts/utils/BaseUtils.java
+++ app/src/main/java/com/an/inshorts/utils/BaseUtils.java
@@ -1,6 +1,12 @@
 package com.an.inshorts.utils;
 
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
 public class BaseUtils {
 
-
+    public static void showSnackBar(String message,
+                                    View view) {
+        Snackbar.make(view, message,Snackbar.LENGTH_LONG).show();
+    }
 }
